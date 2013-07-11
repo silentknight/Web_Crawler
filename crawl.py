14c08fb20f1a55e1e3cb2a46392c5650cc34fa79
silentknight <abhijit65@gmail.com>
2013-07-12 00:19:37 +0530

    Adding more features

--- crawl.py
+++ crawl.py
@@ -1,48 +1,34 @@
 #!/usr/bin/python
 
 import time
-from HTMLParser import HTMLParser
 import pycurl
 import cStringIO
 import bs4
 
-class MyHTMLParser(HTMLParser):
 
-	def __init__(self):
-    		HTMLParser.__init__(self)
-    		self.recording = 0 
-    		self.data = []
-		self.links = []
+def getLinks(page_soup):
 
-  	def handle_starttag(self, tag, attrs):
-    		if tag == 'a':
-          		self.recording = 1
+	text = []
+	links = []
 
-			for attr in attrs:
-				if attr[0] == "href":
-            				self.links.append(attr[1])
+	for link in page_soup.find_all('a'):
+    		links.append(link.get('href'))
+		text.append(link.get_text())
 
-  	def handle_endtag(self, tag):
-    		if tag == 'a':
-      			self.recording -=1 
-
-  	def handle_data(self, data):
-    		if self.recording:
-      			self.data.append(data)
+	return links, text
 
 def explore(url):
 
-	parser = MyHTMLParser()
- 
+	file = open("vistited_links.dat","a")
+
 	buf = cStringIO.StringIO()
 
 	c = pycurl.Curl()
-	c.setopt(c.URL, 'en.wikipedia.org/wiki/Main_Page')
+	c.setopt(c.URL, url)
 	c.setopt(c.CONNECTTIMEOUT, 5)
 	c.setopt(c.TIMEOUT, 20)
 	c.setopt(c.FAILONERROR, True)
 	c.setopt(c.HTTPHEADER, ['Accept: text/html', 'Accept-Charset: UTF-8'])
-	#c.setopt(c.VERBOSE, True)
 	c.setopt(c.WRITEFUNCTION, buf.write)
 	try:
     		c.perform()
@@ -55,34 +41,37 @@ def explore(url):
 	response = buf.getvalue()
 	buf.close()
 
-	parser.feed(response)
+	print response
 
-	print len(parser.data)
-	print parser.data
-	print len(parser.links)
-	print parser.links
+	soup = bs4.BeautifulSoup(response)
+	data = getLinks(soup)
+	links = data[0]
+	text = data[1]
 
-	#for cont in parser.data:
-	#	cont = cont.strip()
-		
-		#if cont[len(cont)-1] == "/":
-		#	urlnew = url + cont
-		#	print urlnew
-		#	explore(urlnew)
-		#else:
-		#	temp = cont.split(".")
-		#	if temp[len(temp) - 1] == "jpg":
-		#		urlImg = url+cont
-		#		print urlImg
-		#		img = urllib2.urlopen(urlImg)
-		#		f = open(cont,"wb")
-		#		f.write(img.read())
-		#		f.close()
+	#for link in links:
+	#	if link != None:
+	#		print link
+	#		file.write(link+"\n")
+	#		explore(link)
+
+			#if cont[len(cont)-1] == "/":
+			#	urlnew = url + cont
+			#	print urlnew
+			#	explore(urlnew)
+			#else:
+			#	temp = cont.split(".")
+			#	if temp[len(temp) - 1] == "jpg":
+			#		urlImg = url+cont
+			#		print urlImg
+			#		img = urllib2.urlopen(urlImg)
+			#		f = open(cont,"wb")
+			#		f.write(img.read())
+			#		f.close()
 
 def main():
 	print "Start rolling..."
 
-	baseAddress = "http://www.linuxquestions.org/"
+	baseAddress = "http://en.wikipedia.org/wiki/Main_Page"
 
 	explore(baseAddress)	
 
